81faa931581b6675d2c7aa06c4dd903bbc671c4e
gadalindra <73198617+gadalindra@users.noreply.github.com>
2021-05-03 22:21:12 +0300

    Add files via upload

---
+++ password-generator.py
@@ -0,0 +1,23 @@
+from random import choice
+import string
+
+while True:
+    try:
+        lenght = int(input('Lenght of your password: '))
+        if lenght > 0:
+            break
+        else:
+            print("No valid integer! Please try again ...")
+            continue
+    except ValueError:
+        print("No valid integer! Please try again ...")
+
+def passwordGenerator(lenght: int) -> int:
+    characters = string.ascii_lowercase + string.ascii_uppercase + string.digits
+    password = ""
+    
+    for s in range(lenght):
+        password += choice(characters)
+    print(f"Your password is: {password}")
+
+passwordGenerator(lenght)
